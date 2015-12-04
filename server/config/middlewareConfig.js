28c35e906485ae3cca510bfcd7fa595097b5537e
Simon Painter <sjpainter19_uk@yahoo.co.uk>
2015-12-04 09:21:16 +0000

    restoring some changes that got lost somehow

--- server/config/middlewareConfig.js
+++ server/config/middlewareConfig.js
@@ -2,11 +2,39 @@ var express = require('express');
 var stylus = require('stylus');
 var morgan = require('morgan');
 var bodyParser = require("body-parser");
+var falcor = require('falcor');
 var falcorExpress = require('falcor-express');
 var falcorRouter = require('falcor-router');
 //var passport = require('passport');
 //var cookieParser = require('cookie-parser');
 var session = require("express-session");
+
+var model = new falcor.Model({
+  cache: {
+    pieces: {
+      "piece1": {
+        composer: "Hook",
+        title: "Minuetto: 2nd movt from Sonata in Eb, Op. 99 No. 3, arr. Wastall"
+      },
+      "piece2": {
+        composer: "Purcell",
+        title: "Rigaudon, Z. 653, arr. Stuart"
+      },
+      "piece3": {
+        composer: "Trad. Irish",
+        title: "The Rakes o’ Mallow, arr. Denley"
+      },
+      "piece4": {
+        composer: "Keith Amos",
+        title: "Lupin, the Pot-Bellied Pig: No. 9"
+      },
+      "piece5": {
+        composer: "Ros Stephen",
+        title: "Guanabara Bay"
+      }
+    }
+  }
+});
 
 module.exports = function(app, config) {
 
@@ -15,13 +43,11 @@ module.exports = function(app, config) {
   }
 
   app.set('views', config.rootPath + '/server/views');
-  app.set('view engine','jade');
-  app.use(stylus.middleware(
-    {
-      src: config.rootPath + '/public',
-      compile: compile
-    }
-  ));
+  app.set('view engine', 'jade');
+  app.use(stylus.middleware({
+    src: config.rootPath + '/public',
+    compile: compile
+  }));
   app.use(morgan('dev'));
   //app.use(cookieParser());
   //app.use(session({secret: 'multi vision unicorns', resave: true, saveUninitialized: true}));
@@ -31,17 +57,9 @@ module.exports = function(app, config) {
     extended: true
   }));
   app.use(bodyParser.json());
-//  app.use(express.static(config.rootPath + '/public'));
+  //  app.use(express.static(config.rootPath + '/public'));
 
-  app.use('/api/model.json', falcorExpress.dataSourceRoute(function(req,res) {
-    return new falcorRouter([
-      {
-        route: "pieces[{keys: pieceId}]",
-        get: function(pieceId) {
-
-        }
-      }
-    ]);
+  app.use('/model.json', falcorExpress.dataSourceRoute(function(req, res) {
+    return model.asDataSource();
   }));
-
 }
